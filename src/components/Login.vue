e84a63e9a44064f1217d4a98731a07683eb54722
gabrieladaltoe <gabrieladaltoe@gmail.com>
2020-01-18 15:37:04 -0300

    create components

---
+++ src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <b-container class="bv-example-row">
+      <b-row align-h="center" class="mt-5">
+        <b-card class="text-center" >
+          <b-form @submit.prevent="onSubmit" @reset.prevent="onReset" >
+            <b-form-group
+              id="input-group-1"
+              label="Email:"
+              label-for="input-1"
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Seu email"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Senha:" label-for="input-2">
+              <b-form-input
+                id="input-2"
+                v-model="form.password"
+                type="password"
+                required
+                placeholder="Sua senha"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-4">
+              <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+                <b-form-checkbox value="me">Lembrar de mim</b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Entrar</b-button>
+          </b-form>
+        </b-card>
+      </b-row>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            form: {
+                    email: '',
+                    password: '',
+                    checked: false
+                },       
+            }
+        },
+        methods: {
+            onSubmit() {
+            alert(JSON.stringify(this.form))
+            },
+            onReset() {
+            this.form.email = ''
+            this.form.name = ''
+            this.form.checked = []
+            }
+        }
+    }
+
+</script>
+
+<style>
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+    
+    <b-container>
+       <h2 align-h="left">Cadastro</h2>
+       <b-card class="text-center form-cadastro"  >
+        <div>
+          <b-form inline class="justify-content-center" >
+            <label class="sr-only" for="inline-form-input-name">Name</label>
+            <b-input
+              id="inline-form-input-name"
+              class="mb-2 mr-sm-2 mb-sm-0"
+              placeholder="Jane Doe"
+            ></b-input>
+
+            <label class="sr-only" for="inline-form-input-username">Username</label>
+            <b-input-group prepend="@" class="mb-2 mr-sm-2 mb-sm-0">
+              <b-input id="inline-form-input-username" placeholder="Username"></b-input>
+            </b-input-group>
+            <b-button variant="primary">Save</b-button>
+          </b-form>
+        </div>
+
+       </b-card>
+       <div>
+          <b-table striped hover :items="items" :fields="fields"></b-table>
+       </div>
+    </b-container>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                fields: ['Nome', 'email' ],
+                items: [
+                { Nome: 'Dickerson Macdonald', email: 'macdonald@email.com' },
+                { Nome: 'Larsen Shaw', email: 'shaw@email.com' },
+                { Nome: 'Geneva Wilson', email: 'wilson@email.com' },
+                { Nome: 'Jami Carney', email: 'carney@email.com' }
+                ]
+            }
+        }
+    } 
+</script>
+
+<style>
+</style>
+
